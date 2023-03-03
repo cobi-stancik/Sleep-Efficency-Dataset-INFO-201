4e0dd5c8ebca0583d48c0c3aaa1af9198027dcaf
“cobi-stancik” <stancikcobi@gmail.com>
2023-03-03 14:43:10 -0800

    v.1 update

--- sleep_efficiency/server.R
+++ sleep_efficiency/server.R
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-
->>>>>>> 02aab2eed0adf268fc420add5acdbd350daf055e
 sleep <- read_delim("../data/Sleep_Efficiency.csv")
 
 library(shiny)
@@ -9,16 +5,11 @@ library(tidyverse)
 
 function(input, output, session) {
   
-<<<<<<< HEAD
   sample <- reactive({
-=======
-  ageSample <- reactive({
->>>>>>> 02aab2eed0adf268fc420add5acdbd350daf055e
     sleep[input$range[1]:input$range[2], ] %>% 
       select(Age, input$percentage) %>% 
       arrange(Age)
   })
-<<<<<<< HEAD
   
   output$table <- renderTable({
     sample()
@@ -34,15 +25,6 @@ function(input, output, session) {
     ggplot(data = sleep_ef(), aes(x = Age, y = `Sleep efficiency`)) +
       geom_point(color = "black", size = 3) + 
       labs(x = "Age", y = "Sleep Efficiency")
-  })
-  
-=======
-
-  output$table <- renderTable({
-      ageSample()
 
   })
-    
-
->>>>>>> 02aab2eed0adf268fc420add5acdbd350daf055e
 }
--- sleep_efficiency/ui.R
+++ sleep_efficiency/ui.R
@@ -2,7 +2,7 @@ library(shiny)
 library(tidyverse)
 
 fluidPage(
-<<<<<<< HEAD
+
   tabsetPanel(
     
     tabPanel(
@@ -59,62 +59,6 @@ fluidPage(
     
     tabPanel(
       "Least Efficient Sleepers"
-=======
-    tabsetPanel(
-      
-      tabPanel(
-        "Project Overview"
-      ),
-      
-      tabPanel(
-        "Analyze Sleep Efficiency"
-      ),
-      
-      tabPanel(
-        "Analyze different age groups based on sleep percentages",
-        sidebarLayout(
-          sidebarPanel(
-            p("Analyze different age groups:"),
-            sliderInput("range", "Age range:",
-                        min = 9,
-                        max = 69,
-                        value = c(21, 35)),
-            checkboxGroupInput("percentage", "Select attribute",
-                               choices = c("REM sleep percentage", "Deep sleep percentage", "Light sleep percentage"),
-                               selected = "REM sleep percentage"),
-            p("The table shown on the right compares the sleep percentages of REM, Deep, and Light sleep
-              for people of varying age groups\n"),
-            p("There is a slight decrease in the percentage of light sleep as age increases, then the percentage begins 
-            to increase once past middle age. People of ages 9-18 had an average of 38.6% light sleep, while people ages
-            30-40 had an average of 19.7%, and people ages 56-65 had an average of 32.6%")
-          ),
-          mainPanel(
-            tableOutput("table")
-          )  
-        )
-      ),
-      
-      tabPanel(
-        "Most Efficient Sleepers"
-      ),
-      
-      tabPanel(
-        "Coffee vs. Alcohol Sleep Efficiency",
-        sidebarLayout(
-          sidebarPanel(
-            p("Analyze sleep efficiency based on coffee and alcohol consumption")
-          ),
-          mainPanel(
-            
-          )
-        )
-      ),
-      
-      tabPanel(
-        "Least Efficient Sleepers"
-      ),
-      
->>>>>>> 02aab2eed0adf268fc420add5acdbd350daf055e
     )
     
   )
