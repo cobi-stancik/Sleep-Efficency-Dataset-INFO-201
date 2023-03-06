3c0865f78ff0b55d598d4b6e5e5b0f508c6aa654
“cobi-stancik” <stancikcobi@gmail.com>
2023-03-05 16:14:38 -0800

    Awakenings Data update

--- sleep_efficiency/server.R
+++ sleep_efficiency/server.R
@@ -1,12 +1,13 @@
+
 sleep <- read_delim("../data/Sleep_Efficiency.csv")
 
 library(shiny)
 library(tidyverse)
 
 function(input, output, session) {
-  
+  # Analyze different age groups based on sleep percentages
   sample <- reactive({
-    sleep[input$range[1]:input$range[2], ] %>% 
+    sleep[input$range2[1]:input$range2[2], ] %>% 
       select(Age, input$percentage) %>% 
       arrange(Age)
   })
@@ -15,6 +16,48 @@ function(input, output, session) {
     sample()
   })
   
+  # Coffee vs. Alcohol Sleep Efficiency
+  coffeeSample <- reactive({
+    sleep %>% 
+      filter(`Caffeine consumption` %in% input$caffeine)
+  })
+  
+  alcoholSample <- reactive({
+    sleep %>% 
+      filter(`Alcohol consumption` %in% input$alcohol)
+  })
+  
+  output$coffeePlot <- renderPlot({
+    coffeeSample() %>% 
+      ggplot(aes(`Sleep efficiency`, `Caffeine consumption`, fill = `Sleep efficiency`)) +
+      geom_col() +
+      ggtitle("Caffeine vs. Sleep efficiency")
+  })
+  
+  output$alcPlot <- renderPlot({
+    alcoholSample() %>% 
+      ggplot(aes(`Sleep efficiency`, `Alcohol consumption`, fill = `Sleep efficiency`)) +
+      geom_col() +
+      ggtitle("Alcohol vs. Sleep efficiency")
+  })
+  
+  # Awakenings Sleep Efficiency
+  awakeSample <- reactive({
+    sleep %>% 
+      filter(`Awakenings` %in% input$awake)
+  })
+  
+  output$awakePlot <- renderPlot({
+    awakeSample() %>% 
+      ggplot(aes(`Sleep efficiency`, `Awakenings`, fill = `Awakenings`)) +
+      geom_col() +
+      ggtitle("Awakenigs vs. Sleep efficiency") +
+      scale_fill_gradient(low="blue", high="red")
+  })
+  
+  
+  
+  # Analyze Sleep Efficiency
   sleep_ef <- reactive({
     sleep[input$range[1]:input$range[2], ] %>% 
       select(Age, `Sleep efficiency`) %>% 
@@ -24,8 +67,23 @@ function(input, output, session) {
   output$sleep <- renderPlot({
     ggplot(data = sleep_ef(), aes(x = Age, y = `Sleep efficiency`)) +
       geom_point(color = "black", size = 3) + 
-      labs(x = "Age", y = "Sleep Efficiency") +
-      xlim(input$range[1], input$range[2])
+      labs(x = "Age", y = "Sleep Efficiency") 
+  })
+  
+  # Overview image
+  output$sleeping_guy <- renderImage({
+    list(src = "../www/sleeping_guy.png",
+         width = "700", 
+         height = "100%",
+         style = "border:5px solid black;")
+  }, deleteFile = F)
+  
+  # URL link to data set
+  url <- a("Sleep Efficiency Data Set", href="https://www.kaggle.com/datasets/equilibriumm/sleep-efficiency")
+  output$tab <- renderUI({
+    tagList("Link to Data Set: ", url)
   })
   
 }
+
+
--- sleep_efficiency/ui.R
+++ sleep_efficiency/ui.R
@@ -2,18 +2,78 @@ library(shiny)
 library(tidyverse)
 
 fluidPage(
-
+  
   tabsetPanel(
     
     tabPanel(
-      "Project Overview"
+      "Project Overview", ## PROJECT OVERVIEW
+      
+      h1(strong("Project Overview"), align = "center"),
+      
+      tags$figure(
+        
+        align = "center",
+        
+        imageOutput("sleeping_guy"),
+        
+        h2(strong("Project Aim🎯")), # Project Aim
+        
+        tags$div(
+          style = "display:block; background-color: lightblue; border:1px solid black; padding:10px;",
+          "This webpage aims to study how various lifestyle choices such as coffee, alcohol, 
+            smoking, and exercise affect the efficiency of sleep. We will be observing the sleep 
+            efficiency of each person by age and gender."
+        ),
+        
+        h2(strong("About the Data Set📊")), # Data Set
+        
+        tags$div(
+          style = "display:block; background-color: lightblue; border:1px solid black; padding:10px;",
+          "We chose to work with a dataset that contains information about the sleep patterns of people 
+            of varying age groups. We accessed this data from kaggle.com. The data came from a sleep study 
+            conducted by a research team from the University of Oxfordshire, and was meant to analyze 
+            the way that sleep can be impacted by different lifestyle factors.",
+          br(), 
+          br(),
+          "The target audience includes male and female subjects, with age ranging from 9 to 69 years 
+            old. The research team recruited participants from the local community in the UK and observed 
+            their sleep patterns over a span of several months.",
+          br(),
+          br(),
+          uiOutput("tab"), # URL link to data set
+        ),
+        
+        h2(strong("Major Questions❔")), # Major Questions
+        
+        tags$div(
+          style = "display:block; background-color: lightblue; border:1px solid black; padding:10px;",
+          "Which lifestyle factor had the most impact on sleep efficiency?"
+        ),
+        
+        br(), # space
+        
+        tags$div(
+          style = "display:block; background-color: lightblue; border:1px solid black; padding:10px;",
+          "How do individual variables in regards to their lifestyle affect REM sleep duration?"
+        ),
+        
+        br(), # space
+        
+        tags$div(
+          style = "display:block; background-color: lightblue; border:1px solid black; padding:10px;",
+          "How does age affect sleep efficiency? (REM sleep, Deep sleep, Light sleep percentages)"
+        ),
+        
+        br() # space
+        
+      )
     ),
     
     tabPanel(
-      "Analyze Sleep Effeciency",
+      "Analyze Sleep Efficiency", ## ANALYZE SLEEP EFFICIENCY
       sidebarLayout(
         sidebarPanel(
-          p("Analyze Sleep Effeciency:"),
+          p("Analyze Sleep Efficiency:"),
           sliderInput("range", "Age range:",
                       min = 9,
                       max = 69,
@@ -26,11 +86,11 @@ fluidPage(
     ),
     
     tabPanel(
-      "Analyze different age groups based on sleep percentages",
+      "Analyze different age groups based on sleep percentages", ## ANALYZE BY AGE
       sidebarLayout(
         sidebarPanel(
           p("Analyze different age groups:"),
-          sliderInput("range", "Age range:",
+          sliderInput("range2", "Age range:",
                       min = 9,
                       max = 69,
                       value = c(21, 35)),
@@ -48,18 +108,50 @@ fluidPage(
         )  
       )
     ),
-    
+  
     tabPanel(
-      "Most Efficient Sleepers"
+      "Coffee vs. Alcohol Sleep Efficiency", ## COFFEE VS ALCOHOL
+      sidebarLayout(
+        sidebarPanel(
+          p("Analyze sleep efficiency based on coffee and alcohol consumption"),
+          fluidRow(
+            column(6,
+                   checkboxGroupInput("caffeine", "Caffeine intake:",
+                                      choices = c(0.0, 25.0, 50.0, 75.0, 100.0, 200.0),
+                                      selected = 25.0)),
+            column(6,
+                   checkboxGroupInput("alcohol", "Alcohol intake:",
+                                      choices = c(0.0, 1.0, 2.0, 3.0, 4.0, 5.0),
+                                      selected = 1.0))
+          )
+        ),
+        mainPanel(
+          plotOutput("coffeePlot"),
+          plotOutput("alcPlot")
+        )
+      )
     ),
     
     tabPanel(
-      "Coffee vs. Alcohol Sleep Efficiency"
+      "Awakeninngs & Sleep Efficiency", ## COFFEE VS ALCOHOL
+      sidebarLayout(
+        sidebarPanel(
+          p("Analyze sleep efficiency based on awakenings"),
+          fluidRow(
+            column(5,
+                   checkboxGroupInput("awake", "Awakenings:",
+                                      choices = c(0.0, 1.0, 2.0, 3.0, 4.0),
+                                      selected = 1.0)),
+            p("The table shown on the right compares the the awakenings a user had in one night in 
+              relation to sleep effecincey, showing the direct correlation between the amount of time
+              you wake up in your sleep to how well you sleep.\n"),
+
+          )
+        ),
+        mainPanel(
+          plotOutput("awakePlot")
+        )
+      )
     ),
-    
-    tabPanel(
-      "Least Efficient Sleepers"
-    )
-    
   )
 )
